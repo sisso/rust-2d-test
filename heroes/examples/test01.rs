ba9f1ecfd931949dca35f11482e926f3b882c845
sisso <sisso@sisso.com.br>
2020-05-04 21:08:33 +0200

    basic polygon collision

--- heroes/examples/test01.rs
+++ heroes/examples/test01.rs
@@ -1,7 +1,7 @@
 use cgmath::Point2;
 use cgmath::{prelude::*, vec2, vec3, Deg, Euler, Quaternion, Rad, Vector2, VectorSpace};
 use ggez::conf::WindowMode;
-use ggez::event::{self, EventHandler, KeyCode, KeyMods};
+use ggez::event::{self, Button, EventHandler, KeyCode, KeyMods, MouseButton};
 use ggez::graphics::Color;
 use ggez::{graphics, timer, Context, ContextBuilder, GameResult};
 use myelin_geometry::*;
@@ -19,6 +19,7 @@ const HEIGHT: u32 = 600;
 struct Cfg {
     update_next: bool,
     speed_reduction: f32,
+    target: Point2<f32>,
 }
 
 impl Cfg {
@@ -26,6 +27,7 @@ impl Cfg {
         Cfg {
             update_next: true,
             speed_reduction: 0.9,
+            target: Point2::new(0.0, 0.0),
         }
     }
 }
@@ -40,11 +42,22 @@ struct MovingArea {
     polygons: Vec<Polygon>,
 }
 
+fn to_point(p2: Point2<f32>) -> Point {
+    (p2.x as f64, p2.y as f64).into()
+}
+
+impl MovingArea {
+    pub fn is_valid(&self, point: Point2<f32>) -> bool {
+        self.polygons
+            .iter()
+            .any(|polygon| polygon.contains_point(to_point(point)))
+    }
+}
+
 #[derive(Clone, Debug, Component)]
 struct Mob {
     pos: Point2<f32>,
     speed: f32,
-    target_pos: Point2<f32>,
 }
 
 #[derive(Clone, Debug, Component)]
@@ -70,7 +83,7 @@ impl App {
         world.insert(Cfg::new());
         world.insert(GameTime { delta_time: 0.01 });
 
-        let border = 20.0;
+        let border = 160.0;
         world.insert(MovingArea {
             polygons: vec![Polygon::try_new(vec![
                 (border, border).into(),
@@ -81,12 +94,54 @@ impl App {
             .unwrap()],
         });
 
+        // {
+        //     let paths: Vec<Vec<f32>> = vec![
+        //         vec![
+        //             0.0, 287.73959, 3.96875, -17.19792, 26.458333, -3.96875, -1.322916, 19.84375,
+        //             -15.875, 10.58334,
+        //         ],
+        //         vec![
+        //             30.427083, 266.57292, 10.583333, 1.32291, 6.614583, 25.13542, -3.96875,
+        //             2.64583, 31.75, 2.64583, -2.645834, -9.26041,
+        //         ],
+        //     ];
+        //
+        //     let mut min: [f32; 2] = [0.0, 0.0];
+        //     let mut max: [f32; 2] = [0.0, 0.0];
+        //     let mut polygons: Vec<Polygon> = vec![];
+        //
+        //     for path in paths {
+        //         let mut vertices: Vec<Point> = vec![];
+        //
+        //         for i in 0..(path.len() / 2) {
+        //             let x = path[i * 2];
+        //             let y = path[i * 2 + 1];
+        //
+        //             max[0] = max[0].max(x);
+        //             max[1] = max[1].max(y);
+        //             min[0] = min[0].min(x);
+        //             min[1] = min[1].min(y);
+        //
+        //             vertices.push((x as f64, y as f64).into());
+        //         }
+        //
+        //         vertices.push(vertices[0].clone());
+        //
+        //         let polygon = match Polygon::try_new(vertices.clone()) {
+        //             Ok(polygon) => polygon,
+        //             Err(e) => panic!("fail to generate a polygon {:?} from {:?}", e, vertices),
+        //         };
+        //         polygons.push(polygon);
+        //     }
+        //
+        //     world.insert(MovingArea { polygons });
+        // }
+
         world
             .create_entity()
             .with(Mob {
                 pos: Point2::new(WIDTH as f32 / 2.0, HEIGHT as f32 / 2.0),
-                speed: 50.0,
-                target_pos: Point2::new(50.0, 100.0),
+                speed: 200.0,
             })
             .with(Model {
                 size: 3.0,
@@ -106,14 +161,15 @@ impl<'a> System<'a> for MobMoviementSystem {
         ReadExpect<'a, GameTime>,
         ReadExpect<'a, Cfg>,
         WriteStorage<'a, Mob>,
+        ReadExpect<'a, MovingArea>,
     );
 
-    fn run(&mut self, (game_time, cfg, mut mobs): Self::SystemData) {
+    fn run(&mut self, (game_time, cfg, mut mobs, moving_area): Self::SystemData) {
         use specs::Join;
 
         for (mob) in (&mut mobs).join() {
             let mob: &mut Mob = mob;
-            let delta = mob.target_pos - mob.pos;
+            let delta = cfg.target - mob.pos;
             let distance = delta.magnitude();
             if distance < 0.1 {
                 println!("complete");
@@ -122,7 +178,12 @@ impl<'a> System<'a> for MobMoviementSystem {
                 let speed = mob.speed.min(distance * cfg.speed_reduction);
                 let vel = dir * speed;
                 let change = vel * game_time.delta_time;
-                mob.pos = mob.pos + change;
+                let new_pos = mob.pos + change;
+
+                if moving_area.is_valid(new_pos) {
+                    mob.pos = new_pos;
+                }
+
                 // println!("{:?} set vel {:?}", entity, movable);
             }
         }
@@ -204,6 +265,12 @@ impl EventHandler for App {
         graphics::draw(ctx, &text, (cgmath::Point2::new(0.0, 0.0), graphics::WHITE))?;
 
         graphics::present(ctx)
+    }
+
+    fn mouse_button_up_event(&mut self, ctx: &mut Context, button: MouseButton, x: f32, y: f32) {
+        if MouseButton::Right == button {
+            self.world.write_resource::<Cfg>().target = (x, y).into();
+        }
     }
 
     fn key_up_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods) {
